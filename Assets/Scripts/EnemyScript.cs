65ab88067b500be32aeb7d1d224fed0f130863ca
Lendon <ranierlendon11@gmail.com>
2026-03-30 20:40:13 +0800

    implement pathfinding

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,14 +4,47 @@ public class EnemyScript : MonoBehaviour
 {
     [SerializeField] private float health = 100f;
 
+    private Transform currentWaypoint;
+    private int currentWaypointIndex = 0;
+    private Transform targetWaypoint;
+
+
+    [SerializeField] private GameObject waypoints;
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        // Get the rigidbody component
+        rb = GetComponent<Rigidbody>();
+
+        // Find the waypoints object
+        waypoints = GameObject.Find("Waypoints");
+
+        // Set initial waypoints
+        currentWaypoint = waypoints.transform.GetChild(currentWaypointIndex);
+        targetWaypoint = waypoints.transform.GetChild(currentWaypointIndex + 1);
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.MovePosition(transform.position + FindPath() * Time.fixedDeltaTime);
+
+        // Checks if the enemy has reached the target waypoint
+        if (Vector3.Distance(transform.position, targetWaypoint.position) < 0.2f)
+        {
+            // Sets the target waypoint to current waypoint
+            currentWaypoint = targetWaypoint;
+            // Increments the current waypoint index
+            currentWaypointIndex++;
+            // Sets the target waypoint to the next waypoint
+            targetWaypoint = waypoints.transform.GetChild(currentWaypointIndex + 1);
+        }
+    }
+
+    private Vector3 FindPath()
+    {
+        Vector3 direction = (targetWaypoint.position - transform.position).normalized;
+        return direction;
     }
 
     public void TakeDamage(float damage)
@@ -27,6 +60,4 @@ public class EnemyScript : MonoBehaviour
     {
         Destroy(gameObject);
     }
-
-    
 }
