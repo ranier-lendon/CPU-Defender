30fb7c65382039fa9b531f78c36949a2667236d3
Lendon <ranierlendon11@gmail.com>
2026-04-16 14:00:48 +0800

    feat: added enum for MalwareType

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,5 +2,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    public enum MalwareType 
+    {
+        Virus = 0,
+        Trojan = 1,
+        Worm = 2,
+        Ransomware = 3,
+        Spyware = 4
+    }
+
+
 }
